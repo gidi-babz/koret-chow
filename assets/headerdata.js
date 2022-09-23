061418a193e233bec669b43a5326e83e1316d3bd
Gidi Babz <tunex4ind@gmail.com>
2022-09-23 12:04:18 +0100

    navbar

---
+++ assets/headerdata.js
@@ -0,0 +1,26 @@
+export default [
+	{
+		path: 'home',
+		label: 'Home',
+	},
+	{
+		path: 'dishes',
+		label: 'Dishes',
+	},
+	{
+		path: 'about',
+		label: 'About',
+	},
+	{
+		path: 'menu',
+		label: 'Menu',
+	},
+	{
+		path: 'review',
+		label: 'Review',
+	},
+	{
+		path: 'order',
+		label: 'Order',
+	},
+];
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
+import { Link } from 'react-scroll';
+import { ImSearch } from 'react-icons/im';
+import { AiFillHeart } from 'react-icons/ai';
+import { FaShoppingCart } from 'react-icons/fa';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Logo from './Logo';
+import headerdata from '../assets/headerdata';
 
 const Navbar = () => {
 	return (
@@ -21,20 +26,34 @@ const Navbar = () => {
 				>
 					<Logo />
 
-					<ul className="flex flex-1 w-3/4 items-center justify-center gap-8 p-3 mr-6">
-						<li>Home</li>
-						<li>Dishes</li>
-						<li>About</li>
-						<li>Menu</li>
-						<li>Review</li>
-						<li>Order</li>
+					<ul className="flex flex-1 w-3/4 items-center justify-center gap-5 p-3 mr-6">
+						{headerdata.map((item, i) => (
+							<Link
+								className="cursor-pointer font-medium  px-2 py-1 rounded-sm hover:text-green-600 active:bg-green-600 active:text-white"
+								key={i}
+								activeClass="active"
+								to={item.path}
+								spy={true}
+								smooth={true}
+								offset={-70}
+								duration={500}
+							>
+								{item.label}
+							</Link>
+						))}
 					</ul>
 
-					<ul className="flex flex-initial items-center justify-center gap-5 mr-2">
-						<li>Icon 1</li>
-						<li>Icon 2</li>
-						<li>Icon 3</li>
-					</ul>
+					<div className="flex items-center justify-center gap-5 mr-2">
+						<div className="bg-slate-100 rounded-full p-2 hover:drop-shadow cursor-pointer">
+							<ImSearch />
+						</div>
+						<div className="bg-slate-100 rounded-full p-2 hover:drop-shadow cursor-pointer">
+							<AiFillHeart />
+						</div>
+						<div className="bg-slate-100 rounded-full p-2 hover:drop-shadow cursor-pointer">
+							<FaShoppingCart />
+						</div>
+					</div>
 				</Box>
 			</Container>
 		</nav>
