2f446506765496a1205bbd25021b2e5997b73e5b
Gidi Babz <tunex4ind@gmail.com>
2022-09-24 02:11:42 +0100

    finished drawer

--- components/drawer.js
+++ components/drawer.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+import Link from 'next/link';
+
 import { Box, Drawer, IconButton } from '@mui/material';
 import { Scrollbars } from 'react-custom-scrollbars';
 import { IoMenuOutline } from 'react-icons/io5';
 import { MdOutlineCancel } from 'react-icons/md';
+import {
+	AiOutlineTwitter,
+	AiOutlineGithub,
+	AiFillLinkedin,
+} from 'react-icons/ai';
 
 import headerdata from '../assets/headerdata';
-import { Link } from 'react-scroll';
+import { Link as NavLink } from 'react-scroll';
 import Logo from './Logo';
 import { GiKnifeFork } from 'react-icons/gi';
 
@@ -46,7 +53,7 @@ const Sidebar = () => {
 				<Scrollbars autoHide>
 					<div className="flex flex-col justify-content align-items gap-4 h-full mt-6 mx-3">
 						{headerdata.map((item, i) => (
-							<Link
+							<NavLink
 								className="cursor-pointer font-medium px-2 py-1 rounded-sm hover:text-green-600 active:bg-green-600 active:text-white active:ease-in border-b-2 border-solid border-slate-inherit"
 								key={i}
 								activeClass="active"
@@ -57,8 +64,39 @@ const Sidebar = () => {
 								duration={500}
 							>
 								{item.label}
-							</Link>
+							</NavLink>
 						))}
+
+						{/* REVISIT THIS SECTION AND CLEANUP/REFACTOR THIS CODE*/}
+						<div className="mt-auto flex justify-center items-center gap-4 mb-8">
+							<div className="p-2 rounded-full bg-slate-100">
+								<Link
+									target="_blank"
+									rel="noopener noreferrer"
+									href="https://github.com/gidi-babz"
+								>
+									<AiOutlineGithub className="text-xl" />
+								</Link>
+							</div>
+							<div className="p-2 rounded-full bg-slate-100">
+								<Link
+									target="_blank"
+									rel="noopener noreferrer"
+									href="https://twitter.com/gidi_babz"
+								>
+									<AiOutlineTwitter className="text-xl" />
+								</Link>
+							</div>
+							<div className="p-2 rounded-full bg-slate-100">
+								<Link
+									target="_blank"
+									rel="noopener noreferrer"
+									href="https://linkedin.com/in/babatunde-olowoyeye"
+								>
+									<AiFillLinkedin className="text-xl" />
+								</Link>
+							</div>
+						</div>
 					</div>
 				</Scrollbars>
 			</Drawer>
