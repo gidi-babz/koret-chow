e81783d844913ac5734cc7ff140a45f89aca0d06
Gidi Babz <tunex4ind@gmail.com>
2022-10-03 16:23:50 +0100

    about icons

--- sections/About.js
+++ sections/About.js
@@ -37,15 +37,15 @@ const About = () => {
 						</p>
 						<div className="flex flex-row flex-wrap justify-start items-center gap-3 mt-3 mb-4">
 							<div className="flex justify-start items-center gap-2 mr-3 lg:mr-2 mb-3 lg:mb-2 shadow-md py-2 px-4 bg-slate-100 rounded-sm">
-								<TbTruckDelivery className="text-xl text-green-600" />
+								<TbTruckDelivery className="text-xl text-green-600 mr-1" />
 								<span className="text-xs about__desc--text">Free Delivery</span>
 							</div>
 							<div className="flex justify-start items-center gap-2 mr-3 lg:mr-2 mb-3 lg:mb-2 shadow-md py-2 px-4 bg-slate-100 rounded-sm">
-								<MdPayment className="text-xl text-green-600" />
+								<MdPayment className="text-xl text-green-600 mr-1" />
 								<span className="text-xs about__desc--text">Easy Payments</span>
 							</div>
 							<div className="flex justify-start items-center gap-2 shadow-md py-2 px-4 mb-3 lg:mb-2 bg-slate-100 rounded-sm">
-								<RiCustomerService2Fill className="text-xl text-green-600" />
+								<RiCustomerService2Fill className="text-xl text-green-600 mr-1" />
 								<span className="text-xs about__desc--text">24/7 Service</span>
 							</div>
 						</div>
